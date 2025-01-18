15408a191873277d0cd54408c9541911356230be
technostrife <technostrife@gmail.com>
2025-01-18 23:01:13 +0300

    25 current  2025-01-18 23:01:11  25.05.20241219.d70bd19     6.6.66                          *

--- nixos/home.nix
+++ nixos/home.nix
@@ -30,7 +30,6 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-    pkgs.gitAndTools.gh
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
